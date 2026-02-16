ddc135c2bc6f943ba3b525b24bfcc59647b59e9a
Gaurav Balpande <gauravbalpande18@gmail.com>
2026-02-16 10:00:14 +0530

    last-09

--- frontend/config.sample.js
+++ frontend/config.sample.js
@@ -8,7 +8,8 @@ window.APP_CONFIG = {
   COGNITO_REGION: 'us-east-1',
   // Required for Login (Hosted UI). Use stack output "CognitoDomain" (e.g. cloud-inventory-app.auth.us-east-1.amazoncognito.com)
   // Or set COGNITO_DOMAIN_PREFIX instead (e.g. cloud-inventory-app) and we build the domain from region.
-  COGNITO_DOMAIN: 'cloud-inventory-app.auth.us-east-1.amazoncognito.com',
+  // COGNITO_DOMAIN: 'cloud-inventory-app.auth.us-east-1.amazoncognito.com',
+  COGNITO_DOMAIN: 'inventory-login-gaurav.auth.us-east-1.amazoncognito.com'
   // COGNITO_DOMAIN_PREFIX: 'cloud-inventory-app'
 };
 
